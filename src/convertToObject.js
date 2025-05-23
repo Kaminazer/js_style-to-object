470794282f819d97918582dab7f3c542ffdd5193
Nazarii Korol <nazarkorol3684@gmail.com>
2025-05-23 13:15:41 +0300

    Add solution for task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssProperties = sourceString.split(';');
+  const result = {};
+
+  cssProperties.forEach((element) => {
+    const [key, value] = element.split(':');
+
+    if (key && value) {
+      const trimmedKey = key.trim();
+      const trimmedValue = value.trim();
+
+      result[trimmedKey] = trimmedValue;
+    }
+  });
+
+  return result;
 }
 
 module.exports = convertToObject;
